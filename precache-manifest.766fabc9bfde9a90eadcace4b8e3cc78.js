5f88d3e78a37d3b4ea82702b7d280f6b603296df
Irina Fylyppova (via Travis CI) <irina.h.fylyppova@gmail.com>
2020-05-09 15:50:08 +0000

    Deploy garevna/dgtek-land-2 to github.com/garevna/dgtek-land-2.git:gh-pages

---
+++ precache-manifest.766fabc9bfde9a90eadcace4b8e3cc78.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc59473ad63398e5bfac6bcf5017e30",
+    "url": "/dgtek-land-2/browserconfig.xml"
+  },
+  {
+    "revision": "5c6fb0a264507dd362ab",
+    "url": "/dgtek-land-2/css/app.c9ec950a.css"
+  },
+  {
+    "revision": "ecc4eefbb8541411413b",
+    "url": "/dgtek-land-2/css/chunk-vendors.76b4a11a.css"
+  },
+  {
+    "revision": "32750b00d6f5003097a67377a4af208f",
+    "url": "/dgtek-land-2/fonts/Gilroy-Bold.32750b00.woff"
+  },
+  {
+    "revision": "3cf0ee273a0b3f022234b6572c3b78f9",
+    "url": "/dgtek-land-2/fonts/Gilroy-Bold.3cf0ee27.ttf"
+  },
+  {
+    "revision": "5e111ca086414e1d8905e5a875fb6899",
+    "url": "/dgtek-land-2/fonts/Gilroy-Bold.5e111ca0.eot"
+  },
+  {
+    "revision": "335c7ebfe69e97fec5ae32fea4fd17fa",
+    "url": "/dgtek-land-2/fonts/Gilroy-Heavy.335c7ebf.eot"
+  },
+  {
+    "revision": "8d36efeb3349af073647c0c37ec995f1",
+    "url": "/dgtek-land-2/fonts/Gilroy-Heavy.8d36efeb.ttf"
+  },
+  {
+    "revision": "ffa5800ec1a44bef80af3dfdced37e7a",
+    "url": "/dgtek-land-2/fonts/Gilroy-Heavy.ffa5800e.woff"
+  },
+  {
+    "revision": "6444f14adcdee041b62184f13139a56d",
+    "url": "/dgtek-land-2/fonts/Gilroy-Medium.6444f14a.ttf"
+  },
+  {
+    "revision": "7d5dc1791cf2738d55a87e871549004c",
+    "url": "/dgtek-land-2/fonts/Gilroy-Medium.7d5dc179.woff"
+  },
+  {
+    "revision": "ce12e7f23ff25c6f534364a7435885df",
+    "url": "/dgtek-land-2/fonts/Gilroy-Medium.ce12e7f2.eot"
+  },
+  {
+    "revision": "59525ce19c9e4999bc19d96e4a88c1b3",
+    "url": "/dgtek-land-2/fonts/Gilroy-Regular.59525ce1.eot"
+  },
+  {
+    "revision": "8e8705d069fbb9099b3a1e1d779fa5c5",
+    "url": "/dgtek-land-2/fonts/Gilroy-Regular.8e8705d0.woff"
+  },
+  {
+    "revision": "ae5e7255973ffe09b53f07a2805232a8",
+    "url": "/dgtek-land-2/fonts/Gilroy-Regular.ae5e7255.ttf"
+  },
+  {
+    "revision": "2d0a0d8f5f173be15a67aa084db94fe6",
+    "url": "/dgtek-land-2/fonts/materialdesignicons-webfont.2d0a0d8f.eot"
+  },
+  {
+    "revision": "b4917be25082eb793b5363f2fdb5f282",
+    "url": "/dgtek-land-2/fonts/materialdesignicons-webfont.b4917be2.woff"
+  },
+  {
+    "revision": "d0066537ab6a4c6f8285a5aeb3ba5f09",
+    "url": "/dgtek-land-2/fonts/materialdesignicons-webfont.d0066537.woff2"
+  },
+  {
+    "revision": "f51112347be6b44f9ef46151a971430d",
+    "url": "/dgtek-land-2/fonts/materialdesignicons-webfont.f5111234.ttf"
+  },
+  {
+    "revision": "cba76bc5ec80591e5d9a55306c1ad32a",
+    "url": "/dgtek-land-2/img/globe.cba76bc5.svg"
+  },
+  {
+    "revision": "3b41e4009573d7f6aadfe9de808aaaa8",
+    "url": "/dgtek-land-2/img/lady_on_sofa.3b41e400.svg"
+  },
+  {
+    "revision": "ada5061dc20107863af0d42cbf567dda",
+    "url": "/dgtek-land-2/img/logo.ada5061d.png"
+  },
+  {
+    "revision": "462a4c0827b3786291b16e3cc4a13e0f",
+    "url": "/dgtek-land-2/img/man_with_building.462a4c08.svg"
+  },
+  {
+    "revision": "54759fbf60e33612e5aafeb3a165b3a8",
+    "url": "/dgtek-land-2/img/map-picture.54759fbf.svg"
+  },
+  {
+    "revision": "365f2efda4e4d916f2e8cfd94a0cd057",
+    "url": "/dgtek-land-2/img/security.365f2efd.svg"
+  },
+  {
+    "revision": "c9a0c7cf57e15eccdb6148411aaec0c9",
+    "url": "/dgtek-land-2/img/speed.c9a0c7cf.svg"
+  },
+  {
+    "revision": "eba356a8c97e579ea85d85ec2aa9089b",
+    "url": "/dgtek-land-2/index.html"
+  },
+  {
+    "revision": "5c6fb0a264507dd362ab",
+    "url": "/dgtek-land-2/js/app.234729d9.js"
+  },
+  {
+    "revision": "ecc4eefbb8541411413b",
+    "url": "/dgtek-land-2/js/chunk-vendors.09c42f34.js"
+  },
+  {
+    "revision": "158f6f0909b196d32bdb1b42d5cd2b95",
+    "url": "/dgtek-land-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dgtek-land-2/robots.txt"
+  }
+]);
